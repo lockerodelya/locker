80cf1f7c921b2af40cd328e0572d60c85ad8cfb4
lockerodelya <care.ompl@gmail.com>
2026-01-21 20:52:46 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker for Odelya (NO PUSH NOTIFICATIONS)
-const CACHE_NAME = 'odelya-cache-v3.1';
+const CACHE_NAME = 'odelya-cache-v3.2';
 const urlsToCache = [
   '/',
   '/index.html',
