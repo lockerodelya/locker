8cbc3606990d83fb0085636ca77d79d8f6600af1
lockerodelya <care.ompl@gmail.com>
2026-03-23 21:42:54 +0530

    Update biology-ecology-ecosystems-level-2.js

--- vooo-ai/subjects/biology-ecology-ecosystems/engine-code/biology-ecology-ecosystems-level-2.js
+++ vooo-ai/subjects/biology-ecology-ecosystems/engine-code/biology-ecology-ecosystems-level-2.js
@@ -6,8 +6,8 @@
 // ⭐⭐⭐ EDIT ONLY THESE 3 LINES FOR A NEW ENGINE ⭐⭐⭐
 // ============================================
 
-const _ENGINE_JSON_FILE     = 'biology-ecology-ecosystems-2.json';
-const _ENGINE_CATEGORY_KEY  = 'biology-ecology-ecosystems-2';
+const _ENGINE_JSON_FILE     = 'biology-ecology-ecosystems-level-2.json';
+const _ENGINE_CATEGORY_KEY  = 'biology-ecology-ecosystems-level-2';
 const _ENGINE_INSTANCE_NAME = 'biologyecologyecosystemsl2';
 
 // ============================================
