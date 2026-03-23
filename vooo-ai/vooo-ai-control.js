e38e2ebea961052cdf5768110cf5d399c2a2d6e1
lockerodelya <care.ompl@gmail.com>
2026-03-23 13:38:26 +0530

    Update vooo-ai-control.js

--- vooo-ai/vooo-ai-control.js
+++ vooo-ai/vooo-ai-control.js
@@ -156,9 +156,9 @@ const VOOO_ENGINE_REGISTRY = {
 	'cs-networks-level-2' : '/vooo-ai/subjects/cs-networks/engine-code/cs-networks-level-2.js',
 	'cs-networks-level-3' : '/vooo-ai/subjects/cs-networks/engine-code/cs-networks-level-3.js',
 
-	'cs-data-reprentation-level-1' : '/vooo-ai/subjects/cs-data-reprentation/engine-code/cs-data-reprentation-level-1.js',
-	'cs-data-reprentation-level-2' : '/vooo-ai/subjects/cs-data-reprentation/engine-code/cs-data-reprentation-level-2.js',
-	'cs-data-reprentation-level-3' : '/vooo-ai/subjects/cs-data-reprentation/engine-code/cs-data-reprentation-level-3.js',
+	'cs-data-representation-level-1' : '/vooo-ai/subjects/cs-data-representation/engine-code/cs-data-representation-level-1.js',
+	'cs-data-representation-level-2' : '/vooo-ai/subjects/cs-data-representation/engine-code/cs-data-representation-level-2.js',
+	'cs-data-representation-level-3' : '/vooo-ai/subjects/cs-data-representation/engine-code/cs-data-representation-level-3.js',
 	
     // --- ADD NEW ENGINES BELOW THIS LINE ---
 };
@@ -315,9 +315,9 @@ const VOOO_ENGINE_INSTANCES = {
 	'cs-networks-level-2' : 'csnetworksl2',
 	'cs-networks-level-3' : 'csnetworksl3',
 
-	'cs-data-reprentation-level-1' : 'csdatarepresentationl1',
-	'cs-data-reprentation-level-2' : 'csdatarepresentationl2',
-	'cs-data-reprentation-level-3' : 'csdatarepresentationl3',
+	'cs-data-representation-level-1' : 'csdatarepresentationl1',
+	'cs-data-representation-level-2' : 'csdatarepresentationl2',
+	'cs-data-representation-level-3' : 'csdatarepresentationl3',
 	
 	
     // --- ADD NEW ENGINES BELOW THIS LINE ---
